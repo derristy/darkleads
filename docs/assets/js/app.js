4629ece1094c460a219371099871fc75fd740fab
Derristy <derristy@gmail.com>
2023-03-17 19:53:52 +0200

    something

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,5 +1,31 @@
-var input = document.querySelector("#tel");
-window.intlTelInput(input, {
-  separateDialCode: true,
-  initialCountry:"ua",
+$(document).ready(() => {
+  var input = document.querySelector("#tel");
+  window.intlTelInput(input, {
+    separateDialCode: true,
+    initialCountry:"ua",
+  });
+  
+  let modalToggle = (id) => {
+    $(id).toggleClass("show");
+  }
+  
+  $("#play-video").on("click", () => {
+    modalToggle("#video-modal");
+  });
+  
+  $(".modal").on("click", (event) => {
+    modalToggle(event.target);
+  });
+
+  $(".modal-dialog").on("click", (event) => {
+    event.stopPropagation();
+  });
+
+  $(".modal-close").on("click", (event) => {
+    modalToggle(event.target.closest(".modal"));
+  });
+
+  $("#problem-arrow").on("click", () => {
+    $('body, html').animate({scrollTop: $(".soluzione").offset().top});
+  });
 });
